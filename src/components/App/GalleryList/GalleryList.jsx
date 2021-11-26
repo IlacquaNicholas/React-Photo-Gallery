f5e08a3db715d1cee660703bc89cb4119fae2f6d
Nicholas Ilacqua <Niservices88@gmail.com>
2021-11-26 12:59:03 -0600

    added images

---
+++ src/components/App/GalleryList/GalleryList.jsx
@@ -0,0 +1,13 @@
+    function GalleryList(){
+        return(
+        <div> 
+            <p>Gallery goes here</p>
+        <img src="images/goat_small.jpg"/>
+                <img src="images/large-Grizzlys.jpeg"/>
+                <img src= 'images/Wolf-hollowing.jpeg'/>
+                <img src= "images/kitten-510651.jpeg" />
+        </div>
+        )
+
+    }
+    export default GalleryList;
